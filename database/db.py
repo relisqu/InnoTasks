abb3f00e29001196c895744d0cc715942dd63c50
Kopeikina Anna <kopkopkop47@gmail.com>
2024-04-27 18:15:04 +0300

    now tests work

--- database/db.py
+++ database/db.py
@@ -1,89 +1,118 @@
 import os
 import sqlite3
 
-conn = sqlite3.connect('data.db', check_same_thread=False)
-c = conn.cursor()
+class Database:
+    def __init__(self, db_path='data.db'):
+        self.db_path = db_path
+        self.conn = sqlite3.connect(self.db_path, check_same_thread=False)
+        self.c = self.conn.cursor()
+        self.create_table()
+
+    def create_table(self):
+        self.c.execute('''
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY,
+                username TEXT UNIQUE,
+                password_hash TEXT
+            )
+        ''')
+
+        self.c.execute('''
+            CREATE TABLE IF NOT EXISTS taskstable (
+                id INTEGER PRIMARY KEY,
+                user_id INTEGER,
+                task TEXT,
+                task_status TEXT,
+                task_due_date DATE,
+                FOREIGN KEY(user_id) REFERENCES users(id)
+            )
+        ''')
+        self.conn.commit()
+
+    def create_table(self):
+        self.c.execute('CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, username TEXT UNIQUE, password_hash TEXT)')
+
+        self.c.execute(
+            'CREATE TABLE IF NOT EXISTS taskstable(id INTEGER PRIMARY KEY, user_id INTEGER, task TEXT, task_status TEXT, '
+            'task_due_date DATE, FOREIGN KEY(user_id) REFERENCES users(id))')
 
 
-def create_table():
-    c.execute('CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, username TEXT UNIQUE, password_hash TEXT)')
-
-    c.execute(
-        'CREATE TABLE IF NOT EXISTS taskstable(id INTEGER PRIMARY KEY, user_id INTEGER, task TEXT, task_status TEXT, '
-        'task_due_date DATE, FOREIGN KEY(user_id) REFERENCES users(id))')
-
-
-def get_user_by_username(username):
-    c.execute('SELECT * FROM users WHERE username=?', (username,))
-    user = c.fetchone()
-    print(user)
-    return user
-
-
-def register_user(username, password_hash):
-    user = get_user_by_username(username)
-    print(f"reg: {user}")
-    if user:
-        return False
-    c.execute('INSERT INTO users(username, password_hash) VALUES (?, ?)', (username, password_hash))
-    conn.commit()
-    return get_user_by_username(username)
-
-
-def authenticate_user(username, password_hash):
-    user = get_user_by_username(username)
-    print(f"authenticate_user init vals: {username}, {password_hash}")
-    print(f"authenticate_user try auth: {user}")
-    if not user:
-        return False
-    if user[2] == password_hash:
+    def get_user_by_username(self, username):
+        self.c.execute('SELECT * FROM users WHERE username=?', (username,))
+        user = self.c.fetchone()
+        print(user)
         return user
-    return False
+
+    def delete_user(self, username):
+        self.c.execute('DELETE FROM users WHERE username=?', (username,))
+        self.conn.commit()
+
+    def delete_user_tasks(self, user_id):
+        self.c.execute('DELETE FROM taskstable WHERE user_id = ?', (user_id,))
+        self.conn.commit()
+    def register_user(self, username, password_hash):
+        user = self.get_user_by_username(username)
+        print(f"reg: {user}")
+        if user:
+            return False
+        self.c.execute('INSERT INTO users(username, password_hash) VALUES (?, ?)', (username, password_hash))
+        self.conn.commit()
+        return self.get_user_by_username(username)
 
 
-def add_data(user_id, task, task_status, task_due_date):
-    c.execute('INSERT INTO taskstable(user_id, task, task_status, task_due_date) VALUES (?, ?, ?, ?)',
-              (user_id, task, task_status, task_due_date))
-    conn.commit()
+    def authenticate_user(self, username, password_hash):
+        user = self.get_user_by_username(username)
+        print(f"authenticate_user init vals: {username}, {password_hash}")
+        print(f"authenticate_user try auth: {user}")
+        if not user:
+            return False
+        if user[2] == password_hash:
+            return user
+        return False
 
 
-def view_all_data(user_id):
-    c.execute('SELECT * FROM taskstable WHERE user_id=?', (user_id,))
-    data = c.fetchall()
-    print(f'view all data: {data}')
-    return data
+    def add_data(self, user_id, task, task_status, task_due_date):
+        self.c.execute('INSERT INTO taskstable(user_id, task, task_status, task_due_date) VALUES (?, ?, ?, ?)',
+                  (user_id, task, task_status, task_due_date))
+        self.conn.commit()
 
 
-def view_all_task_names(user_id):
-    c.execute('SELECT DISTINCT task FROM taskstable WHERE user_id=?', (user_id,))
-    data = c.fetchall()
-    return data
+    def view_all_data(self, user_id):
+        self.c.execute('SELECT * FROM taskstable WHERE user_id=?', (user_id,))
+        data = self.c.fetchall()
+        print(f'view all data: {data}')
+        return data
 
 
-def get_task(user_id, task):
-    c.execute('SELECT * FROM taskstable WHERE user_id=? AND task=?', (user_id, task))
-    data = c.fetchall()
-    return data
+    def view_all_task_names(self, user_id):
+        self.c.execute('SELECT DISTINCT task FROM taskstable WHERE user_id=?', (user_id,))
+        data = self.c.fetchall()
+        return data
 
 
-def get_task_by_status(user_id, task_status):
-    c.execute('SELECT * FROM taskstable WHERE user_id=? AND task_status=?', (user_id, task_status))
-    data = c.fetchall()
-    return data
+    def get_task(self, user_id, task):
+        self.c.execute('SELECT * FROM taskstable WHERE user_id=? AND task=?', (user_id, task))
+        data = self.c.fetchall()
+        return data
 
 
-def edit_task_data(user_id, new_task, new_task_status, new_task_date, task, task_status, task_due_date):
-    c.execute(
-        "UPDATE taskstable SET task=?, task_status=?, task_due_date=? WHERE user_id=? AND task=? AND task_status=? AND task_due_date=?",
-        (new_task, new_task_status, new_task_date, user_id, task, task_status, task_due_date))
-    conn.commit()
-    data = c.fetchall()
-    return data
+    def get_task_by_status(self, user_id, task_status):
+        self.c.execute('SELECT * FROM taskstable WHERE user_id=? AND task_status=?', (user_id, task_status))
+        data = self.c.fetchall()
+        return data
 
 
-def delete_data(user_id, task):
-    c.execute('DELETE FROM taskstable WHERE user_id=? AND task=?', (user_id, task))
-    conn.commit()
+    def edit_task_data(self, user_id, new_task, new_task_status, new_task_date, task, task_status, task_due_date):
+        self.c.execute(
+            "UPDATE taskstable SET task=?, task_status=?, task_due_date=? WHERE user_id=? AND task=? AND task_status=? AND task_due_date=?",
+            (new_task, new_task_status, new_task_date, user_id, task, task_status, task_due_date))
+        self.conn.commit()
+        data = self.c.fetchall()
+        return data
 
 
-create_table()
+    def delete_data(self, user_id, task):
+        self.c.execute('DELETE FROM taskstable WHERE user_id=? AND task=?', (user_id, task))
+        self.conn.commit()
+
+
--- tests/test_database.py
+++ tests/test_database.py
@@ -1,69 +1,62 @@
 import unittest
-from database.db import *
 from unittest.mock import patch
 from unittest.mock import MagicMock
 import sqlite3
-
-
+from database.db import *
 class TestDatabaseFunctions(unittest.TestCase):
 
     def setUp(self):
         # Connect to an in-memory database for testing
-        self.conn = sqlite3.connect(':memory:')
-        self.cursor = self.conn.cursor()
-        create_table()
+        self.database = Database(':memory:')
 
-    def tearDown(self):
-        # Close the database connection after each test
-        self.conn.close()
 
     def test_delete_user(self):
-        self.clear_test_user()
-        register_user("test_user", "password123")
-        delete_user('test_user')
-        user_after_deletion = get_user_by_username('test_user')
+        self.database.register_user("test_user", "password123")
+        self.database.delete_user('test_user')
+        user_after_deletion = self.database.get_user_by_username('test_user')
         self.assertIsNone(user_after_deletion, "User should not exist after deletion")
 
     def test_delete_user_tasks_and_check_existence(self):
 
         self.clear_test_user()
 
-        user = register_user("test_user", "password123")
-        add_data(user[0], 'Task 1', 'ToDo', '2024-04-30')
-        add_data(user[0], 'Task 2', 'Doing', '2024-05-01')
+        user = self.database.register_user("test_user", "password123")
+        self.database.add_data(user[0], 'Task 1', 'ToDo', '2024-04-30')
+        self.database.add_data(user[0], 'Task 2', 'Doing', '2024-05-01')
 
-        delete_user_tasks(user[0])
-        tasks_after_deletion = view_all_data(user[0])
+        self.database.delete_user_tasks(user[0])
+        tasks_after_deletion = self.database.view_all_data(user[0])
         self.assertEqual(len(tasks_after_deletion), 0, "No tasks should exist after deletion")
+
     def test_register_user(self):
         # Test user registration
         self.clear_test_user()
-        self.assertTrue(register_user("test_user", "password123"))
-        self.assertFalse(register_user("test_user", "password123"))  # Duplicate registration should fail
+        self.assertTrue(self.database.register_user("test_user", "password123"))
+        self.assertFalse(self.database.register_user("test_user", "password123"))  # Duplicate registration should fail
 
     def test_authenticate_user(self):
         # Test user authentication
         self.clear_test_user()
-        register_user("test_user", "password123")
-        self.assertTrue(authenticate_user("test_user", "password123"))
-        self.assertFalse(authenticate_user("test_user", "wrong_password"))
+        self.database.register_user("test_user", "password123")
+        self.assertTrue(self.database.authenticate_user("test_user", "password123"))
+        self.assertFalse(self.database.authenticate_user("test_user", "wrong_password"))
 
     def test_add_and_view_task(self):
         # Test adding and viewing tasks
         self.clear_test_user()
-        user = register_user("test_user", "password123")
-        add_data(user[0], "Test Task", "ToDo", "2024-05-01")
-        tasks = view_all_data(user[0])
-        self.assertEqual(1,len(tasks) )
+        user = self.database.register_user("test_user", "password123")
+        self.database.add_data(user[0], "Test Task", "ToDo", "2024-05-01")
+        tasks = self.database.view_all_data(user[0])
+        self.assertEqual(1, len(tasks))
         self.assertEqual(tasks[0][2], "Test Task")
 
     def test_edit_task(self):
         # Test editing a task
         self.clear_test_user()
-        user = register_user("test_user", "password123")
-        add_data(user[0], "Test Task", "ToDo", "2024-05-01")
-        edit_task_data(user[0], "Updated Task", "Done", "2024-05-02", "Test Task", "ToDo", "2024-05-01")
-        tasks = view_all_data(user[0])
+        user = self.database.register_user("test_user", "password123")
+        self.database.add_data(user[0], "Test Task", "ToDo", "2024-05-01")
+        self.database.edit_task_data(user[0], "Updated Task", "Done", "2024-05-02", "Test Task", "ToDo", "2024-05-01")
+        tasks = self.database.view_all_data(user[0])
         self.assertEqual(tasks[0][2], "Updated Task")
         self.assertEqual(tasks[0][3], "Done")
         self.assertEqual(tasks[0][4], "2024-05-02")
@@ -71,41 +64,40 @@ class TestDatabaseFunctions(unittest.TestCase):
     def test_delete_task(self):
         # Test deleting a task
         self.clear_test_user()
-        user = register_user("test_user", "password123")
-        add_data(user[0], "Test Task", "ToDo", "2024-05-01")
-        delete_data(user[0], "Test Task")
-        tasks = view_all_data(user[0])
+        user = self.database.register_user("test_user", "password123")
+        self.database.add_data(user[0], "Test Task", "ToDo", "2024-05-01")
+        self.database.delete_data(user[0], "Test Task")
+        tasks = self.database.view_all_data(user[0])
         self.assertEqual(len(tasks), 0)
 
     def test_get_task_by_status(self):
         # Test getting tasks by status
         self.clear_test_user()
-        user = register_user("test_user", "password123")
-        add_data(user[0], "Test Task 1", "ToDo", "2024-05-01")
-        add_data(user[0], "Test Task 2", "Done", "2024-05-02")
-        tasks = get_task_by_status(user[0], "Done")
+        user = self.database.register_user("test_user", "password123")
+        self.database.add_data(user[0], "Test Task 1", "ToDo", "2024-05-01")
+        self.database.add_data(user[0], "Test Task 2", "Done", "2024-05-02")
+        tasks = self.database.get_task_by_status(user[0], "Done")
         self.assertEqual(len(tasks), 1)
         self.assertEqual(tasks[0][2], "Test Task 2")
 
     def test_get_task_by_username(self):
         # Test getting user by username
         self.clear_test_user()
-        register_user("test_user", "password123")
-        user = get_user_by_username("test_user")
+        self.database.register_user("test_user", "password123")
+        user = self.database.get_user_by_username("test_user")
         self.assertIsNotNone(user)
         self.assertEqual(user[1], "test_user")
 
     def test_get_task(self):
         # Test getting a task by task name
         self.clear_test_user()
-        user = register_user("test_user", "password123")
-        add_data(user[0], "Test Task", "ToDo", "2024-05-01")
-        task = get_task(user[0], "Test Task")
+        user = self.database.register_user("test_user", "password123")
+        self.database.add_data(user[0], "Test Task", "ToDo", "2024-05-01")
+        task = self.database.get_task(user[0], "Test Task")
         self.assertIsNotNone(task)
         self.assertEqual(task[0][2], "Test Task")
 
     def clear_test_user(self):
-        user_id = get_user_by_username("test_user")
-        if user_id is not None:
-            delete_user_tasks(user_id[0])
-            delete_user("test_user")
+        print("god help us all")
+if __name__ == '__main__':
+    unittest.main()

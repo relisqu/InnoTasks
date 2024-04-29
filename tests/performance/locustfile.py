f34bac40037d7a627d6d5db7f2249d92ea39b141
Eliza <lizavetta.vl@yandex.ru>
2024-04-29 18:37:23 +0300

    y

--- tests/performance/locustfile.py
+++ tests/performance/locustfile.py
@@ -7,13 +7,13 @@ from locust import HttpUser, task, between, events
 @events.quitting.add_listener
 def _(environment, **_):
     if environment.stats.total.fail_ratio > 0.01:
-        logging.error("Test failed due to failure ratio > 1%")
+        logging.error("Test failed: failure ratio > 1%")
         environment.process_exit_code = 1
     elif environment.stats.total.avg_response_time > 200:
-        logging.error("Test failed due to average response time ratio > 200 ms")
+        logging.error("Test failed: average response time ratio > 200 ms")
         environment.process_exit_code = 1
     elif environment.stats.total.get_response_time_percentile(0.95) > 800:
-        logging.error("Test failed due to 95th percentile response time > 800 ms")
+        logging.error("Test failed: 95th percentile response time > 800 ms")
         environment.process_exit_code = 1
     else:
         environment.process_exit_code = 0
@@ -37,8 +37,11 @@ class TestInnoTasks(HttpUser):
     def test_add_task(self):
         # add a task for the user
         self.client.post("/task",
-                         json={"user_id": self.id, "task": "task", "task_status": "task_status",
-                               "task_priority": "task_priority", "task_due_date": "task_due_date"})
+                         json={"user_id": self.id,
+                               "task": "task",
+                               "task_status": "task_status",
+                               "task_priority": "task_priority",
+                               "task_due_date": "task_due_date"})
 
     @task
     def test_get_task(self):
